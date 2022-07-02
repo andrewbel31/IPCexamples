9d0b3cca6ad81770c96706435b5a36ddfcd22fa0
Andrei <andreyrabo@mail.ru>
2022-07-02 14:15:27 +0100

    gitignore

---
+++ app/src/main/java/com/andreibelous/ipcexamples/App.kt
@@ -0,0 +1,11 @@
+package com.andreibelous.ipcexamples
+
+import android.app.Application
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/andreibelous/ipcexamples/ProcessHelper.kt
@@ -0,0 +1,50 @@
+package com.andreibelous.ipcexamples
+
+import android.app.Application
+import android.os.Build
+import android.os.Process
+import android.os.StrictMode
+import java.io.BufferedReader
+import java.io.FileReader
+
+object ProcessHelper {
+
+    /**
+     * @return full name of current process
+     *
+     * Full name looks like `your.app.package:yourProcessName`
+     */
+    @JvmStatic
+    val currentProcessFullName: String?
+        get() {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                Application.getProcessName()
+            } else {
+                readProcessName(Process.myPid())
+            }
+        }
+
+    private fun readProcessName(pid: Int): String? {
+        if (pid <= 0) {
+            return null
+        }
+
+        return runCatching {
+            val bufferedReader: BufferedReader
+            val oldThreadPolicy = StrictMode.allowThreadDiskReads()
+
+            try {
+                bufferedReader = BufferedReader(FileReader("/proc/$pid/cmdline"))
+            } finally {
+                StrictMode.setThreadPolicy(oldThreadPolicy)
+            }
+
+            val processName = bufferedReader.readLine().trim { it <= ' ' }
+
+            runCatching { bufferedReader.close() }
+
+            return@runCatching processName
+        }.getOrNull()
+    }
+
+}
